ebf0cca1545adfe6824527febafc5ff3daa4a302
Sai Mahendra Penumakula <saimahipenumakula@gmail.com>
2026-01-28 13:37:11 -0500

    Inner Bean

---
+++ spring-1st-project/2.13 InnerBean/src/main/java/org/springframeworkxmlbasedconfig/Main.java
@@ -0,0 +1,21 @@
+package org.springframeworkxmlbasedconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // if you want to create a container, we have to use something called an ApplicationContext.Now ApplicationContext is
+        // responsible to work with the IOC container. Actually we do have two options to interact with the container
+        // One is BeanFactory which is an old one, the application context, a new one. Both does the same thing, almost the same thing.
+        // Basically, it will help you to create the container, and it will help you to get the objects from the container
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        Alien alien1= (Alien) context.getBean("alien1");
+        System.out.println(alien1.getAge()); //28
+        alien1.code();
+
+    }
+}
