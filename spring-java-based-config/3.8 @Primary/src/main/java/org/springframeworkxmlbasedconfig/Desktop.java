be5e7bd113c20f7e6decd47120c69ea58b1da03c
Sai Mahendra Penumakula <saimahipenumakula@gmail.com>
2026-01-29 12:46:53 -0500

    Using @Qualifier Along with @Primary

---
+++ spring-java-based-config/3.8 @Primary/src/main/java/org/springframeworkxmlbasedconfig/Desktop.java
@@ -0,0 +1,19 @@
+package org.springframeworkxmlbasedconfig;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Component("computer2")
+@Primary
+public class Desktop implements Computer{
+
+    public Desktop() {
+        System.out.println("Desktop Objected Created...");
+    }
+
+    @Override
+    public void compile() {
+        System.out.println("Compiling in Desktop....");
+    }
+}
