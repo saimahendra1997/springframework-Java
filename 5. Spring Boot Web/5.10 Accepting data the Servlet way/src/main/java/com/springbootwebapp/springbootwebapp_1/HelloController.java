7068edcde3c7d76f01ca1275c7ec28649ae7e1f2
Sai Mahendra Penumakula <165595202+saimahendra1997@users.noreply.github.com>
2026-01-31 23:10:13 -0500

    Change result parameter type from String to int

--- 5. Spring Boot Web/5.10 Accepting data the Servlet way/src/main/java/com/springbootwebapp/springbootwebapp_1/HelloController.java
+++ 5. Spring Boot Web/5.10 Accepting data the Servlet way/src/main/java/com/springbootwebapp/springbootwebapp_1/HelloController.java
@@ -47,7 +47,7 @@ public class HelloController {
     public String resultPage(HttpServletRequest request) {
         int num1 = Integer.parseInt(request.getParameter("num1"));
         int num2 = Integer.parseInt(request.getParameter("num2"));
-        String result = request.getParameter("result");
+        int result = Integer.parseInt(request.getParameter("result"));
         return "result.jsp";
     }
 
